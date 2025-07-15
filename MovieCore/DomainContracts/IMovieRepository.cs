3db809cf4fe0fd977e7ff68cf51f1dc868eaf498
wpplng <lina.wappling@gmail.com>
2025-07-15 15:42:08 +0200

    Repository for Movie

---
+++ MovieCore/DomainContracts/IMovieRepository.cs
@@ -0,0 +1,19 @@
+﻿using MovieCore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCore.DomainContracts
+{
+    public interface IMovieRepository
+    {
+        Task<IEnumerable<Movie>> GetAllAsync();
+        Task<Movie?> GetAsync(int id);
+        Task<bool> AnyAsync(int id);
+        void Add(Movie movie);
+        void Update(Movie movie);
+        void Remove(Movie movie);
+    }
+}
---
+++ MovieData/Repositories/MovieRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCore.DomainContracts;
+using MovieCore.Models.Dtos;
+using MovieCore.Models.Entities;
+using MovieData.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieData.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly MovieContext context;
+
+        public MovieRepository(MovieContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Movie>> GetAllAsync()
+        {
+            return await context.Movies.ToListAsync();
+        }
+
+        public async Task<Movie?> GetAsync(int id)
+        {
+            return await context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+        }
+        public async Task<bool> AnyAsync(int id)
+        {
+            return await context.Movies.AnyAsync(m => m.Id == id);
+        }
+
+        public void Add(Movie movie)
+        {
+            context.Movies.Add(movie);
+        }
+
+        public void Update(Movie movie)
+        {
+            context.Movies.Update(movie);
+        }
+
+        public void Remove(Movie movie)
+        {
+            context.Movies.Remove(movie);
+        }
+    }
+}
