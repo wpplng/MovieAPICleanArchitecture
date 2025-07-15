9f54967a4d8d4822dab18f17b522edcbf93c3cb1
wpplng <lina.wappling@gmail.com>
2025-07-15 16:18:54 +0200

    Repository for actor

---
+++ MovieCore/DomainContracts/IActorRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieCore.Models.Entities;
+
+namespace MovieCore.DomainContracts
+{
+    public interface IActorRepository
+    {
+        Task<Movie?> GetMovieWithActorsAsync(int movieId);
+        Task<Actor?> GetAsync(int actorId);
+        bool ActorExistsInMovie(Movie movie, int actorId);
+        void AddActorToMovie(Movie movie, Actor actor);
+    }
+}
---
+++ MovieData/Repositories/ActorRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCore.DomainContracts;
+using MovieCore.Models.Entities;
+using MovieData.Data;
+
+namespace MovieData.Repositories
+{
+    public class ActorRepository : IActorRepository
+    {
+        private readonly MovieContext context;
+
+        public ActorRepository(MovieContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Movie?> GetMovieWithActorsAsync(int movieId)
+        {
+            return await context.Movies
+                .Include(m => m.Actors)
+                .FirstOrDefaultAsync(m => m.Id == movieId);
+        }
+
+        public async Task<Actor?> GetAsync(int actorId)
+        {
+            return await context.Actors.FindAsync(actorId);
+        }
+
+        public bool ActorExistsInMovie(Movie movie, int actorId)
+        {
+            return movie.Actors.Any(a => a.Id == actorId);
+        }
+
+        public void AddActorToMovie(Movie movie, Actor actor)
+        {
+            movie.Actors.Add(actor);
+        }
+    }
+}
+
