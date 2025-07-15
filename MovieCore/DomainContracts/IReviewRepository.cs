d09064d9c8a742a41ec902fc51da2a2051d306fb
wpplng <lina.wappling@gmail.com>
2025-07-15 16:18:38 +0200

    Repository for review

---
+++ MovieCore/DomainContracts/IReviewRepository.cs
@@ -0,0 +1,10 @@
+﻿using MovieCore.Models.Entities;
+
+namespace MovieCore.DomainContracts
+{
+    public interface IReviewRepository
+    {
+        Task<bool> MovieExistsAsync(int movieId);
+        Task<IEnumerable<Review>> GetByMovieIdAsync(int movieId);
+    }
+}
---
+++ MovieData/Repositories/ReviewRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCore.DomainContracts;
+using MovieCore.Models.Entities;
+using MovieData.Data;
+
+namespace MovieData.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly MovieContext context;
+
+        public ReviewRepository(MovieContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> MovieExistsAsync(int movieId)
+        {
+            return await context.Movies.AnyAsync(m => m.Id == movieId);
+        }
+
+        public async Task<IEnumerable<Review>> GetByMovieIdAsync(int movieId)
+        {
+            return await context.Reviews
+                .Where(r => r.MovieId == movieId)
+                .ToListAsync();
+        }
+    }
+}
